64454833c85cac76727305f526fbc13f315ce75b
Franck Seigneuret <franck.seigneuret@gmail.com>
2021-02-10 22:19:54 +0100

    évite doubons articles dans wishlist

--- reactapp/src/reducers/article.reducer.js
+++ reactapp/src/reducers/article.reducer.js
@@ -2,9 +2,15 @@ export default function (wishList = [], action) {
 
   switch (action.type) {
     case 'addArticle':
-      let wishListCopy = [...wishList]
-      wishListCopy.push(action.articleLike)
-      return wishListCopy
+      const check = wishList.filter((item) => (item.title === action.articleLike.title))
+
+      if(check.length > 0) {
+        return wishList
+      } else {
+        let wishListCopy = [...wishList]
+        wishListCopy.push(action.articleLike)
+        return wishListCopy
+      }
 
       break
 
