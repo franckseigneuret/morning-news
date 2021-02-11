5aadba6ac82b7ec45d6e323bb64638a1a54d8227
Franck Seigneuret <franck.seigneuret@gmail.com>
2021-02-11 13:46:34 +0100

    gestion des langues

--- reactapp/src/ScreenSource.js
+++ reactapp/src/ScreenSource.js
@@ -6,25 +6,40 @@ import Nav from './Nav'
 
 function ScreenSource() {
 
+  const mappingLanguage = {
+    fr: {
+      language: 'fr',
+      country: 'fr',
+    },
+    en: {
+      language: 'en',
+      country: 'gb',
+    },
+  }
   const [sourceList, setSourceList] = useState([])
+  const [language, setLanguage] = useState(mappingLanguage.fr)
 
   useEffect(() => {
     const APIResultsLoading = async () => {
-
-      const data = await fetch('https://newsapi.org/v2/sources?language=fr&country=fr&apiKey=074d1c4547884ebeb0659a460b5d1f37')
+      let url = `https://newsapi.org/v2/sources?language=${language.language}&country=${language.country}&apiKey=074d1c4547884ebeb0659a460b5d1f37`
+      const data = await fetch(url)
       const body = await data.json()
       // console.log(body.sources)
       setSourceList(body.sources)
     }
     APIResultsLoading()
 
-  }, [])
+  }, [language])
 
   return (
     <div>
       <Nav />
 
-      <div className="Banner"></div>
+      <div className="Banner">
+        <img src="https://i.ebayimg.com/images/g/7gQAAOSwv7pb5YIO/s-l300.jpg" onClick={() => setLanguage(mappingLanguage.fr)} />
+        <img src="https://images-na.ssl-images-amazon.com/images/I/419RsYi16FL._AC_.jpg" onClick={() => setLanguage(mappingLanguage.en)} />
+      </div>
+
       <div className="HomeThemes">
         <List
           itemLayout="horizontal"
